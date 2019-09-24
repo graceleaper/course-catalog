f1e0b9ddf94f3c472d0979ddf860dd5813cdd286
Grace Lee <igracelee1@gmail.com>
2019-09-23 23:34:41 -0400

    fetching authors request

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,25 +1,65 @@
 import React from "react";
-// import fetchAuthorData from "../fetchAuthorData";
+import fetchAuthorData from "../fetchAuthorData";
 
-const Courses = props => {
-  // console.log(fetchAuthorData())
+class Courses extends React.Component {
 
-  const allCourses = props.courses.map(course => {
+  state = {
+    authors: []
+  }
+
+  /*
+    - return authors object in fetchAuthors
+    - hashmap --> an array with objects as elements -- each obj will have course id & author name
+    - set state to be an array (the hashmap)
+  */
+
+  fetchAuthors = async () => {
+    const response = await fetchAuthorData.get()
+    // this.setState({authors: response.data.authors})
+    return response.data.authors
+  }
+
+  componentDidMount() {
+    this.defineAuthors()
+  }
+
+  defineAuthors = async () => {
+    const authorsObj = await this.fetchAuthors()
+    let authorIds = Object.keys(authorsObj)
+    let authorNames = []
+    for (let key in authorsObj) {
+      authorNames.push(authorsObj[key])
+    }
+
+    const allAuthorInfo = authorIds.map((id) => {
+      return authorNames.map((name) => {
+        return {id: id, name: name.name}
+      })
+    })
+
+    this.setState({authors: allAuthorInfo})
+    console.log(this.state.authors)
+  }
+
+  render() {
+    const allCourses = this.props.courses.map(course => {
+      return (
+        <div className="col" key={course.id}>
+        <img className="course" alt="course preview" src={course.image} />
+          <h3>{course.title}</h3>
+          <p className="description">{course.description}</p>
+          {course.pro ? <p className="pro">Pro</p> : null}
+
+        </div>
+      );
+    })
+
     return (
-      <div className="col" key={course.id}>
-      <img className="course" alt="course preview" src={course.image} />
-        <h3>{course.title}</h3>
-        <p className="description">{course.description}</p>
-        {course.pro ? <p className="pro">Pro</p> : null}
-      </div>
+        <div className="flex-grid">
+        {allCourses}
+        </div>
     );
-  })
-
-  return (
-      <div className="flex-grid">
-      {allCourses}
-      </div>
-  );
+  }
 };
 
 export default Courses;
--- src/fetchAuthorData.js
+++ src/fetchAuthorData.js
@@ -1,12 +1,28 @@
 // NOTE: this file is only necessary for bonus objective #1
+import axios from 'axios'
 
 const authorEndpoint =
   "https://s3.us-east-2.amazonaws.com/codecademy-interview/entities.json";
 
 // make a GET request to the above endpoint to get author data
+/*
 const fetchAuthorData = async () => {
     const authors = await fetch(authorEndpoint, {mode: 'cors'}).then(response => response.json());
     return authors;
 };
+*/
 
-export default fetchAuthorData;
+/*
+const fetchAuthorData = async () => {
+  const authorsObj = await axios.get(authorEndpoint)
+  //console.log(authorsObj.data.authors)
+  const authors = await authorsObj.data.authors
+  return authors
+}
+*/
+
+export default axios.create({
+  baseURL: authorEndpoint
+})
+
+// export default fetchAuthorData;
